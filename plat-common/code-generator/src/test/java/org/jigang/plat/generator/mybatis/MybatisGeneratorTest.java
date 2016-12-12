b6194046ddef394fd75e51619ec644fa60df9ac7
wjg <huozhanbai@baofu.com>
2016-12-12 11:08:10 +0800

    测试MybatisGenerator

--- plat-common/code-generator/src/test/java/org/jigang/plat/generator/mybatis/MybatisGeneratorTest.java
+++ plat-common/code-generator/src/test/java/org/jigang/plat/generator/mybatis/MybatisGeneratorTest.java
@@ -2,12 +2,14 @@ package org.jigang.plat.generator.mybatis;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
  * Created by wjg on 2016/12/12.
  */
 public class MybatisGeneratorTest {
     @Test
-    public void generate() {
-
+    public void generate() throws IOException {
+        new MybatisGenerator().generator();
     }
 }
